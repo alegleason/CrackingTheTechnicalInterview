9e337472d6659dd25b826d117ac39b0a5161da89
alegleason <A01703013@itesm.mx>
2021-01-25 20:18:28 -0600

    Add new array LC probs

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -1,6 +1,10 @@
+import sys
+
+
 class Solution:
+    # Find Pivot Index - Introduction to Array
     def pivotIndex(self, nums):
-        # O(n) time complexity and O(1) space complexity
+        # O(n) time complexity and O(1) space complexity where n is the length of nums
         S = sum(nums)
         leftSum = 0
 
@@ -12,6 +16,27 @@ class Solution:
 
         return -1
 
+    # Largest Number At Least Twice of Others - Introduction to Array
+    def dominantIndex(self, nums):
+        # O(n) time complexity and O(1) space complexity where n is the length of nums
+        maxNum = sys.maxsize * -1
+        secMaxNum = sys.maxsize * -1
+        maxNumIdx = -1
+
+        for i, num in enumerate(nums):
+            if num > maxNum:
+                secMaxNum = maxNum
+                maxNum = num
+                maxNumIdx = i
+            if maxNum > num > secMaxNum:
+                secMaxNum = num
+        # If the max number is greater or equal than the second max number divided by 2,
+        # it means that it is valid for the whole array, otherwise, invalid
+        if maxNum >= secMaxNum * 2:
+            return maxNumIdx
+        return -1
+
 
 sol = Solution()
 # print(sol.pivotIndex([1,7,3,6,5,6]))
+# print(sol.dominantIndex([0,0,3,2]))
