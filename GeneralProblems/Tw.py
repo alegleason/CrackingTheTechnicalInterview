c13cc6cac3d23d4a873fc93b831be53e75a01b8b
alegleason <A01703013@itesm.mx>
2021-02-16 21:51:48 -0600

    Hard problems

--- GeneralProblems/Tw.py
+++ GeneralProblems/Tw.py
@@ -1,4 +1,6 @@
 from collections import deque
+import enum
+import sys
 
 
 # Given an array of arrival times and duration, determine the max number of events
@@ -22,7 +24,7 @@ def maxEvents(arrival, duration):
             et = times[i][1]
             count += 1
     return count
-    # print(maxEvents([1, 3, 3, 5, 7], [2, 2, 1, 2, 1]))  # 4
+    # print(maxEvents([1, 3, 3, 5, 7], [2, 2, 1, 2, 1]))  -> 4
 
 
 # 907. Sum of Subarray Minimums LeetCode
@@ -48,13 +50,12 @@ def sumSubarrayMins(A):
             count += stack.pop()[1]
         right[i] = count
         stack.append([A[i], count])
-    print(left)
-    print(right)
     # Calculate the results in the form of res = sum(A[i] * f(i))
     res = 0
     for i in range(n):
         res += left[i] * right[i] * A[i]
-    return res
+    return res % mod
+    # print(sumSubarrayMins([3,1,2,4]))# -> 17
 
 
 # 739. Daily Temperatures LeetCode
@@ -63,7 +64,7 @@ def dailyTemperatures(T):
     n = len(T)
     result = [0] * n
     stack = deque()
-    for i in range(n-1, -1, -1):
+    for i in range(n - 1, -1, -1):
         # Pop the stack until we find a greater element than us
         while stack and T[stack[-1]] <= T[i]:
             stack.pop()
@@ -72,25 +73,85 @@ def dailyTemperatures(T):
         stack.append(i)
 
     return result
+    # print(dailyTemperatures([73,74,75,71,69,72,76,73])) -> [1, 1, 4, 2, 1, 1, 0, 0]
 
 
-print(dailyTemperatures([73,74,75,71,69,72,76,73]))
+# Given an array of prices, determine the discount for each price, which is, the next number lesser or equal than it
+def minDiscount(D):
+    n = len(D)
+    # Next Less Elements Indexes help calculate discount
+    discounts = D.copy()
+    stack = deque()
+    notChanged = set()
+    for i in range(n):
+        while stack and D[i] <= D[stack[-1]]:
+            x = stack.pop()
+            # Update all previous indexes that had not found a lesser element
+            discounts[x] = D[x] - D[i]
+            # Get elements without discount
+            notChanged.remove(x)
+        stack.append(i)
+        notChanged.add(i)
 
-# for i in range(n):
-#     count = 1
-#     while stack and stack[-1][0] > A[i]:
-#         count += stack.pop()[1]
-#     left[i] = count
-#     stack.append([A[i], count])
-# stack.clear()
-# for i in range(n - 1, -1, -1):
-#     count = 1
-#     while stack and stack[-1][0] >= A[i]:
-#         count += stack.pop()[1]
-#     right[i] = count
-#     stack.append([A[i], count])
-# res = 0
-# # Calculate the results in the form of res = sum(A[i] * f(i))
-# for i in range(n):
-#     res += A[i] * left[i] * right[i]
-# return res % mod
+    return sum(discounts), notChanged
+    # print(minDiscount([5, 4, 5, 1, 3, 3, 8, 2]))  # -> totalDisc = 18, elemNoDisc = 3, 7
+
+
+class Cell(enum.Enum):
+    Unknown = 1
+    Impossible = 2
+    Possible = 3
+
+
+# 55. Jump Game LeetCode
+def canJump(nums):
+    # O(n^2) time and O(n) space
+    n = len(nums)
+    # Initialize DP structure
+    dp = [Cell.Unknown] * n
+    # Mark last cell as possible (itself)
+    dp[n - 1] = Cell.Possible
+    return canJumpRecursive(0, dp, nums)
+    # print(canJump([2, 3, 1, 1])) -> True
+
+
+def canJumpRecursive(start, dp, nums):
+    # Base case
+    if dp[start] != Cell.Unknown:
+        return True if dp[start] == Cell.Possible else False
+
+    # Recursive calls
+    maxSteps = min(len(nums) - 1, nums[start] + start)
+    for i in range(start + 1, maxSteps + 1):
+        if canJumpRecursive(i, dp, nums):
+            dp[start] = Cell.Possible
+            return True
+
+    dp[start] = Cell.Impossible
+    return False
+
+
+# 45. Jump Game II LeetCode
+def canJumpII(nums):
+    # O(n) time and O(1) space complexity
+    if not nums or len(nums) == 1:
+        return 0
+    # BFS similarity
+    furthest = nums[0]
+    curr_end = nums[0]
+    jumps = 1
+    n = len(nums)
+    # Greedy approach
+    for i in range(1, n):
+        if i == n - 1:
+            return jumps
+        if i + nums[i] > furthest:
+            furthest = i + nums[i]
+        # We have finished processing the level
+        if i == curr_end:
+            jumps += 1
+            curr_end = furthest
+    return jumps
+
+
+print(canJumpII([1,2,3]))
